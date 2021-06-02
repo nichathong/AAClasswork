ba925370fbe76f835c02a8eb7a8f7dd26ba25866
nichathong <nicha.fn@gmail.com>
2021-06-02 16:15:06 -0700

    Added #null_piece

--- W4D2/Chess/null_piece.rb
+++ W4D2/Chess/null_piece.rb
@@ -1,5 +1,16 @@
-class NullPiece
+require "Singleton"
+require_relative "piece.rb"
+class NullPiece < Piece
+
+    include Singleton
+
+    attr_reader :symbol, :color
     def initialize
-        
+        @symbol = " "
+        @color = " "
+    end
+
+    def moves
+        []
     end
 end
--- W4D2/Chess/piece.rb
+++ W4D2/Chess/piece.rb
@@ -6,6 +6,31 @@ class Piece
         @pos = pos
     end
 
+    def to_s
+        "#{symbol}"
+    end
+
+    def empty?
+
+    end
+
+    def symbol
+        raise "Do not use this symbol"
+    end
+
+    def valid_moves
+        moves
+    end
+
+    def pos=(val)
+        self.pos = val
+    end
+
+    private
+
+    def move_into_check?(end_pos)
+    end
+
 
     
 end
