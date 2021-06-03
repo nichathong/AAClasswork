2853bc230d4425219109b5ece05053850f01b53f
nichathong <nicha.fn@gmail.com>
2021-06-03 16:42:11 -0700

    started on initialize

--- W4D4/hanoi_project/lib/hanoi.rb
+++ W4D4/hanoi_project/lib/hanoi.rb
@@ -1,4 +1,6 @@
 class TowerOfHanoi
-
+    def initialize
+        @base = Array.new(3) { Array.new(6) }
+    end
     
 end
--- W4D4/hanoi_project/spec/hanoi_spec.rb
+++ W4D4/hanoi_project/spec/hanoi_spec.rb
@@ -1,5 +1,15 @@
 require "hanoi"
 
 describe TowerOfHanoi do
+
+    describe "#initialize" do
+        it "base needs to be three, and should be able to hold six rings" do
+            subject(:base) { TowerOfHanoi.new }
+            expect(:base.length).to eq(3)
+            expect(:base[0].length).to eq(6)
+
+        end
+
+    end
     
 end
