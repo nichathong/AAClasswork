41215af8764407231e9a16caaae73d8ab76a0490
nichathong <nicha.fn@gmail.com>
2021-06-11 15:02:35 -0700

    Completed Harrison Ford

--- W5D5/skeleton/movie_buff/01_queries.rb
+++ W5D5/skeleton/movie_buff/01_queries.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id          :integer      not null, primary key
+#  name        :string
+#
+# Table name: movies
+#
+#  id          :integer      not null, primary key
+#  title       :string
+#  yr          :integer
+#  score       :float
+#  votes       :integer
+#  director_id :integer
+#
+# Table name: castings
+#
+#  id          :integer      not null, primary key
+#  movie_id    :integer      not null
+#  actor_id    :integer      not null
+#  ord         :integer
+
+
+
+
 def it_was_ok
   # Consider the following:
   #
@@ -6,6 +32,12 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
+  Movie
+    .select(:id, :title, :score)
+    .where(score: (2..3))
+
+
+
 
 end
 
@@ -20,6 +52,12 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .where(actors: { name: 'Harrison Ford'})
+    .where.not(castings: { ord: 1} )
 
 end
 
